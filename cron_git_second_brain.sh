f97fc4fcb501d3aee5ace18606cedb071b608958
Nate Anderson <nanderson@greensea.com>
2025-03-06 09:55:00 -0500

    initial commit

---
+++ cron_git_second_brain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the absolute path to your Git repository
+REPO_PATH="/home/nanderson/nate_personal/Obsidian/second_brain"
+
+# Navigate to the Git repository
+cd "$REPO_PATH" || exit
+
+# Load user environment variables (needed for cron)
+export PATH=/usr/bin:/bin:/usr/local/bin
+
+# Get the current timestamp
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Commit message with timestamp
+commit_message="Auto commit: $timestamp"
+
+# Add, commit, and push changes
+git add .
+git commit -m "$commit_message"
+git push origin master
